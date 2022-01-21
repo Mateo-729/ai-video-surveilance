7605ac4c625896b74705c95118beaafe05256544
Mateo-729 <90667625+Mateo-729@users.noreply.github.com>
2022-01-20 16:54:52 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,58 @@
+
+objects=[];
+status="";
+function preload()
+{
+    video=createVideo('video.mp4')
+}
+function setUp()
+{
+    canvas=createCanvas(480,380);
+    canvas.center();
+    video.hide()
+}
+function draw()
+{
+    image(video,0,0,480,380);
+    console.log("status="+status);
+    if(status!="")
+    {
+        objectDetector.detect(video,gotResult);
+        for(i=0;i<objects.length;i++)
+        {
+            document.getElementById("status").innerHTML="Status : Objects Detected";
+            document.getElementById("number_of_objects").innerHTML="Number of Objects Detected are : "+objects.length;
+
+            fill("red");
+            percent=floor(objects[i].confidence*100);
+            text(objects[i].label+" "+percent+"%",objects[i].x+15,objects[i].y+15);
+            noFill();
+            stroke('black');
+            rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height);
+
+        }
+    }
+}
+function start()
+{
+    objectDetector=ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML="Status : Detecting objects"
+}
+function modelLoaded()
+{
+    console.log("Model loaded!");
+    status=true;
+    video.loop();
+    video.speed(1);
+    video.volume(0);
+}
+function gotResult(error,results)
+{
+    if(error)
+    {
+        console.log(error);
+    }
+    console.log(results);
+    objects=results;
+    
+}
